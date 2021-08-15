06679883f2b0f39e6bcac34d621f67bba016cff6
Variic <VARICLOTHAR@GMAIL.com>
2021-08-15 13:13:16 +0100

    ADD MODYFICATION TO LOOP FOR-OF

--- js/script.js
+++ js/script.js
@@ -5,11 +5,16 @@
   console.log('links:', links);
 });*/
 
-const titleClickHandler = function(event){
-  console.log('Link was clicked!');
-  console.log('event', event);
+  const titleClickHandler = function(event){
+    console.log('Link was clicked!');
+    console.log('event', event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
 
